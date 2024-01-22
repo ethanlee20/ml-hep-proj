86c657f67ee34b0948dc7d9d45d58b8f0df1e0e7
ethanlee20 <elee20@hawaii.edu>
2024-01-23 04:07:47 +0900

    added comments

--- lib/mylib/eff_and_res.py
+++ lib/mylib/eff_and_res.py
@@ -7,21 +7,28 @@ import numpy as np
 
 
 def generate_bin_edges(start, stop, num_of_bins):
+    """Generate histogram bin edges."""
+    
     bin_size = (stop - start) / num_of_bins
     return np.arange(start, stop + bin_size, bin_size)
 
 
 def find_bin_middles(bin_edges):
-    """Assumes uniform bin widths"""
+    """
+    Find the position of the middle of each bin.
+    
+    Note: Assumes uniform bin widths.
+    """
     num_bins = len(bin_edges) - 1
     bin_width = (
         np.max(bin_edges) - np.min(bin_edges)
     ) / num_bins
     shifted_edges = bin_edges + 0.5 * bin_width
     return shifted_edges[:-1]
-
+    
 
 def find_bin_counts(data, binning_variable, bin_edges):
+    """Find the numbers of entries in each bin."""
     bin_series = pd.cut(
         data[binning_variable],
         bin_edges,
@@ -33,6 +40,13 @@ def find_bin_counts(data, binning_variable, bin_edges):
 
 
 def calculate_efficiency(data1, data2, variable, bin_edges):
+    """
+    Calculate the efficiency per bin.
+    
+    The efficiency of bin i is defined as the number of
+    data1 entries in i divided by the number of data2
+    entries in i.
+    """
     bin_counts_data1 = find_bin_counts(
         data1, variable, bin_edges
     )
@@ -45,6 +59,13 @@ def calculate_efficiency(data1, data2, variable, bin_edges):
 def calculate_efficiency_errorbars(
     data1, data2, variable, bin_edges
 ):
+    """
+    Calculate the errorbar of the efficiency for each bin.
+
+    The errorbar for bin i is calculated as the squareroot of the
+    number of data1 entries in i divided by the number of
+    data2 entries in i.
+    """
     bin_counts_data1 = find_bin_counts(
         data1, variable, bin_edges
     )
@@ -57,6 +78,7 @@ def calculate_efficiency_errorbars(
 
 
 def apply_q_squared_split(split, data):
+    """Split the data by q squared value."""
     if split == "med":
         return data[(data['q_squared']>1)&(data['q_squared']<6)]
     elif split == "all":
@@ -65,6 +87,13 @@ def apply_q_squared_split(split, data):
 
 
 def calculate_resolution(variable, q_squared_split, data):
+    """
+    Calculate the resolution.
+    
+    The resolution of a variable is defined as the 
+    reconstructed value minus the MC truth value.
+    """
+
 
     signal_data = data[data["isSignal"]==1]
 
@@ -90,6 +119,7 @@ def calculate_resolution(variable, q_squared_split, data):
 
 
 def calculate_resolutions(variables, q_squared_splits, data):
+    """Calculate the resolutions of multiple variables and q squared regions."""
 
     calc_info = [
         dict(var=var, split=split) 
