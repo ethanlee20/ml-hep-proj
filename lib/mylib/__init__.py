adeb6cd559ed6d603d8f69e7a41453aa22db2690
ethanlee20 <elee20@hawaii.edu>
2024-02-08 12:31:10 -1000

    fix

--- lib/mylib/__init__.py
+++ lib/mylib/__init__.py
@@ -1,14 +1,1 @@
-import os
-import sys
 
-sys.path.insert(0, os.path.dirname(__file__))
-
-from test_df import *
-from util import *
-from eff_and_res import *
-from cuts import *
-
-# import img_gen
-# import maths
-from physics import *
-from plotting import *
