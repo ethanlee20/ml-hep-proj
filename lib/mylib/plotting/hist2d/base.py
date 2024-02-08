1c123cbd67f9367e9dc0c39b5224f3e26eb0cc07
ethanlee20 <elee20@hawaii.edu>
2024-02-08 13:25:02 -1000

    fix

--- lib/mylib/plotting/hist2d/base.py
+++ lib/mylib/plotting/hist2d/base.py
@@ -14,7 +14,7 @@ def titles(num_gen_events, num_det_events):
     return titles
 
 
-def ax_labels(num_gen_events, num_det_events):
+def make_ax_labels(num_gen_events, num_det_events):
     titles = titles(num_gen_events, num_det_events)
     ax_labels = [
         Labels(title=titles["gen"]), 
@@ -48,7 +48,7 @@ def plot_hist_2d(
 
     add_color_bar(axs, norm)
 
-    ax_labels = ax_labels(
+    ax_labels = make_ax_labels(
         find_num_events(data.loc["gen"]), 
         find_num_events(data.loc["det"])
     )
