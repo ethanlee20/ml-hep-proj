4c828f08da008bc01b8c82a16e70e21954f8e789
ethanlee20 <elee20@hawaii.edu>
2024-02-09 11:31:32 -1000

    efficiency

--- scripts/plot/plot.py
+++ scripts/plot/plot.py
@@ -25,18 +25,18 @@ from mylib.plotting.efficiency.costheta_k import (
 
 setup_mpl_params_save()
 
-
-data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
+data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/')
+# data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
 out_dir = pl.Path('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/plots2/')
 
 
-# hist_theta_lab_k(data, out_dir)
+hist_theta_lab_k(data, out_dir)
 
-# hist_2d_costheta_k_theta_k(data, out_dir)
-# hist_2d_costheta_k_p_k(data, out_dir)
+hist_2d_costheta_k_theta_k(data, out_dir)
+hist_2d_costheta_k_p_k(data, out_dir)
 
-# hist_2d_chi_p_k(data, out_dir)
-# hist_2d_chi_theta_k(data, out_dir)
+hist_2d_chi_p_k(data, out_dir)
+hist_2d_chi_theta_k(data, out_dir)
 
 eff_cos_theta_k(data, out_dir)
 eff_cos_theta_k_check_theta_k_accep(data, out_dir)
