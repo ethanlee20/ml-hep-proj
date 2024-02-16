d5ffcb024f3e18e377f24bd782345f3aec6baa1e
ethanlee20 <elee20@hawaii.edu>
2024-02-16 12:17:59 -1000

    fix

--- lib/mylib/plot/afb.py
+++ lib/mylib/plot/afb.py
@@ -19,8 +19,8 @@ def plot_afb(data, out_dir):
     leg_gen = stats_legend(d_cos_theta_mu.loc["gen"], "Generator", show_mean=False, show_rms=False)
     leg_det = stats_legend(d_cos_theta_mu.loc["det"], "Detector", show_mean=False, show_rms=False)
 
-    plt.scatter(*gen, s=2, color="red", alpha=0.6, label=leg_gen, marker="X")
-    plt.scatter(*det, s=2, color="blue", alpha=0.6, label=leg_det, marker="d")
+    plt.scatter(*gen, s=4, color="red", alpha=0.6, label=leg_gen, marker="X")
+    plt.scatter(*det, s=4, color="blue", alpha=0.6, label=leg_det, marker="d")
     plt.ylim(-0.3, 0.5)
     plt.xlim(0, 19) 
     plt.title(r"$A_{fb}$ for $\ell = \mu$")
