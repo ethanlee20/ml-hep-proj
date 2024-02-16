5dcf8bf1246c038416e81828818e4ae5bdbcfa9e
ethanlee20 <elee20@hawaii.edu>
2024-02-16 11:59:49 -1000

    fix

--- lib/mylib/plot/afb.py
+++ lib/mylib/plot/afb.py
@@ -12,15 +12,15 @@ def plot_afb(data, out_dir):
     d_cos_theta_mu = section(data, only_sig=True, var="costheta_mu")
     d_q_squared = section(data, only_sig=True, var="q_squared")
 
-    num_points = 100
+    num_points = 300
     gen = calc_afb_of_q_squared(d_cos_theta_mu.loc["gen"], d_q_squared.loc["gen"], num_points)
     det = calc_afb_of_q_squared(d_cos_theta_mu.loc["det"], d_q_squared.loc["det"], num_points)
     
     leg_gen = stats_legend(d_cos_theta_mu.loc["gen"], "Generator", show_mean=False, show_rms=False)
     leg_det = stats_legend(d_cos_theta_mu.loc["det"], "Detector", show_mean=False, show_rms=False)
 
-    plt.scatter(*gen, color="red", label=leg_gen, marker="X")
-    plt.scatter(*det, color="blue", label=leg_det, marker="d")
+    plt.scatter(*gen, color="red", alpha=0.8, label=leg_gen, marker="X")
+    plt.scatter(*det, color="blue", alpha=0.8, label=leg_det, marker="d")
     plt.title(r"$A_{fb}$ for $\ell = \mu$")
     plt.xlabel(r"$q^2$ [GeV$^2$]")
     plt.ylabel(r"$A_{fb}$")
