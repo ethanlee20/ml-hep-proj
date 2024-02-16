241e7adb19db5ef8949ab595948499b72a7c953f
ethanlee20 <elee20@hawaii.edu>
2024-02-16 09:59:33 -1000

    fix

--- lib/mylib/plot/core/looks/annotate.py
+++ lib/mylib/plot/core/looks/annotate.py
@@ -10,18 +10,25 @@ def make_one_scaler(min, max):
     return scale
 
 
-def annotate_theta_accept(xlim:tuple):
+def make_big_scaler(min, max):
+    def scale(num):
+        return ((max - min) * num) + min
+    return scale
+
+
+def annotate_theta_accept(xlim:tuple, ylim:tuple):
 
     one_scale = make_one_scaler(*xlim)
-    
-    plt.axhline(y=100, xmin=one_scale(radians(17)), xmax=one_scale(radians(150)), label="PXD, SVD, CDC", color="red")
-    plt.axhline(y=200, xmin=one_scale(radians(31)), xmax=one_scale(radians(128)), label="TOP", color="aquamarine")
-    plt.axhline(y=300, xmin=one_scale(radians(15)), xmax=one_scale(radians(34)), label="ARICH", color="pink")
-    plt.axhline(y=400, xmin=one_scale(radians(12.4)), xmax=one_scale(radians(31.4)), label="ECL", color="green", linestyle="--")
-    plt.axhline(y=400, xmin=one_scale(radians(32.2)), xmax=one_scale(radians(128.7)), color="green", linestyle="--")
-    plt.axhline(y=400, xmin=one_scale(radians(130.7)), xmax=one_scale(radians(155.1)), color="green", linestyle="--")
-    plt.axhline(y=500, xmin=one_scale(radians(40)), xmax=one_scale(radians(129)), label="KLM", color="orange", linestyle=":")
-    plt.axhline(y=500, xmin=one_scale(radians(25)), xmax=one_scale(radians(40)), color="orange", linestyle=":")
-    plt.axhline(y=500, xmin=one_scale(radians(129)), xmax=one_scale(radians(155)), color="orange", linestyle=":")
+    big_scale = make_big_scaler(*ylim)
+
+    plt.axhline(y=big_scale(0.1), xmin=one_scale(radians(17)), xmax=one_scale(radians(150)), label="PXD, SVD, CDC", color="red")
+    plt.axhline(y=big_scale(0.2), xmin=one_scale(radians(31)), xmax=one_scale(radians(128)), label="TOP", color="aquamarine")
+    plt.axhline(y=big_scale(0.3), xmin=one_scale(radians(15)), xmax=one_scale(radians(34)), label="ARICH", color="pink")
+    plt.axhline(y=big_scale(0.4), xmin=one_scale(radians(12.4)), xmax=one_scale(radians(31.4)), label="ECL", color="green", linestyle="--")
+    plt.axhline(y=big_scale(0.4), xmin=one_scale(radians(32.2)), xmax=one_scale(radians(128.7)), color="green", linestyle="--")
+    plt.axhline(y=big_scale(0.4), xmin=one_scale(radians(130.7)), xmax=one_scale(radians(155.1)), color="green", linestyle="--")
+    plt.axhline(y=big_scale(0.5), xmin=one_scale(radians(40)), xmax=one_scale(radians(129)), label="KLM", color="orange", linestyle=":")
+    plt.axhline(y=big_scale(0.5), xmin=one_scale(radians(25)), xmax=one_scale(radians(40)), color="orange", linestyle=":")
+    plt.axhline(y=big_scale(0.5), xmin=one_scale(radians(129)), xmax=one_scale(radians(155)), color="orange", linestyle=":")
 
     plt.legend()
