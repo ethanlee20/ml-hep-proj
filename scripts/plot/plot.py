4b2c1802a208c0d12bdec706f750fd41dd964f29
ethanlee20 <elee20@hawaii.edu>
2024-02-16 09:03:43 -1000

    plot

--- scripts/plot/plot.py
+++ scripts/plot/plot.py
@@ -1,26 +1,26 @@
 
 import pathlib as pl
 
-from mylib.utilities.util import open_data, open_data_dir
+from mylib.util.util import open_data, open_data_dir
 
-from mylib.plotting.core.utils.setup import setup_mpl_params_save
+from mylib.plot.core.utils.setup import setup_mpl_params_save
 
-from mylib.plotting.hist1d.theta_lab_k import hist_theta_lab_k
+from mylib.plot.hist1d.theta_lab_k import hist_theta_lab_k
 
-from mylib.plotting.hist2d.costheta_k import (
-    hist_2d_costheta_k_theta_k, 
-    hist_2d_costheta_k_p_k, 
-)
+# from mylib.plot.hist2d.costheta_k import (
+#     hist_2d_costheta_k_theta_k, 
+#     hist_2d_costheta_k_p_k, 
+# )
 
-from mylib.plotting.hist2d.chi import (
-    hist_2d_chi_p_k, 
-    hist_2d_chi_theta_k
-)
+# from mylib.plotting.hist2d.chi import (
+#     hist_2d_chi_p_k, 
+#     hist_2d_chi_theta_k
+# )
 
-from mylib.plotting.efficiency.costheta_k import (
-    eff_cos_theta_k, 
-    eff_cos_theta_k_check_theta_k_accep,
-)
+# from mylib.plotting.efficiency.costheta_k import (
+#     eff_cos_theta_k, 
+#     eff_cos_theta_k_check_theta_k_accep,
+# )
 
 
 setup_mpl_params_save()
@@ -32,13 +32,13 @@ out_dir = pl.Path('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKst
 
 hist_theta_lab_k(data, out_dir)
 
-hist_2d_costheta_k_theta_k(data, out_dir)
-hist_2d_costheta_k_p_k(data, out_dir)
+# hist_2d_costheta_k_theta_k(data, out_dir)
+# hist_2d_costheta_k_p_k(data, out_dir)
 
-hist_2d_chi_p_k(data, out_dir)
-hist_2d_chi_theta_k(data, out_dir)
+# hist_2d_chi_p_k(data, out_dir)
+# hist_2d_chi_theta_k(data, out_dir)
 
-eff_cos_theta_k(data, out_dir)
-eff_cos_theta_k_check_theta_k_accep(data, out_dir)
+# eff_cos_theta_k(data, out_dir)
+# eff_cos_theta_k_check_theta_k_accep(data, out_dir)
 
 
