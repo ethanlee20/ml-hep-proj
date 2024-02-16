3f65e4caa4b452546c4edeb2773768be86cf6f69
ethanlee20 <elee20@hawaii.edu>
2024-02-16 11:56:03 -1000

    fix

--- lib/mylib/calc/afb.py
+++ lib/mylib/calc/afb.py
@@ -27,7 +27,7 @@ def calc_binned_afb(d_cos_theta_l, bins):
 
 
 def calc_afb_of_q_squared(d_cos_theta_l, d_q_squared, num_points):
-    bin_edges = make_bin_edges(start=0, stop=20, num_bins=num_points)
+    bin_edges = make_bin_edges(start=d_q_squared.min(), stop=d_q_squared.max(), num_bins=num_points)
     bins = make_q_squared_bins(d_q_squared, bin_edges)
     afbs = calc_binned_afb(d_cos_theta_l, bins)
     q_squareds = find_bin_middles(bin_edges)
