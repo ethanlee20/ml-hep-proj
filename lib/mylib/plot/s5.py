cdbe56b0475af5a8dc1aa56832f9b5259f86fa3a
ethanlee20 <elee20@hawaii.edu>
2024-02-20 13:53:15 -1000

    fix

--- lib/mylib/plot/s5.py
+++ lib/mylib/plot/s5.py
@@ -13,9 +13,10 @@ def plot_s5(df, out_dir):
     num_points = 30
     gen_q_sq, gen_s5 = calc_s5_of_q_squared(df.loc["gen"], num_points)
     det_q_sq, det_s5 = calc_s5_of_q_squared(df.loc["det"], num_points)
+    breakpoint()
     
-    leg_gen = stats_legend(gen_s5, "Generator", show_mean=False, show_rms=False)
-    leg_det = stats_legend(det_s5, "Detector", show_mean=False, show_rms=False)
+    leg_gen = stats_legend(df["chi"].loc["gen"], "Generator", show_mean=False, show_rms=False)
+    leg_det = stats_legend(df["chi"].loc["det"], "Detector", show_mean=False, show_rms=False)
 
     plt.scatter(gen_q_sq, gen_s5, s=4, color="red", alpha=0.6, label=leg_gen, marker="X")
     plt.scatter(det_q_sq, det_s5, s=4, color="blue", alpha=0.6, label=leg_det, marker="d")
