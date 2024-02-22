5d94126e8c450041bf7aff688ccc52ff4e17075b
ethanlee20 <elee20@hawaii.edu>
2024-02-22 11:07:04 -1000

    analyze

--- scripts/calculate/analyze.py
+++ scripts/calculate/analyze.py
@@ -1,6 +1,7 @@
 
-"""Calculate required quantities from the raw MC data."""
+"""Calculate required quantities from the reconstructed MC data."""
 
+import pathlib as pl
 
 from mylib.util.util import open_data
 from mylib.calc.phys import (
@@ -10,13 +11,35 @@ from mylib.calc.phys import (
     find_costheta_ell,
     four_momemtum_dataframe,
     invariant_mass_squared_two_particles,
-
 )
 
 
-data_path = ''
-out_path = ''
-ell = ''
+input_dir = '/home/belle2/elee20/ml-hep-proj/data/2024-02-21_e_test/cut'
+output_dir = '/home/belle2/elee20/ml-hep-proj/data/2024-02-21_e_test/ana'
+ell = 'e'
+
+
+def config_input_data_paths(input_dir):
+    input_dir = pl.Path(input_dir)
+    input_paths = list(input_dir.glob("*.pkl"))
+    return input_paths
+
+
+def config_output_data_paths(output_dir, input_paths):    
+    output_dir = pl.Path(output_dir)
+    output_paths = [
+        output_dir.joinpath(f"{path.stem}_an.pkl")
+        for path in input_paths
+    ]
+    return output_paths
+
+
+def config_paths(input_dir, output_dir):
+    input_paths = config_input_data_paths(input_dir)
+    output_paths = config_output_data_paths(output_dir, input_paths)
+    return input_paths, output_paths
+
+
 
 
 if ell == 'mu':
@@ -61,85 +84,95 @@ else:
     raise ValueError(f"ell not recognized: {ell}")
 
 
-data = open_data(data_path)
+def run_calc(data):
+    data = data.copy()
 
-df_B_4mom = four_momemtum_dataframe(
-    data[["E", "px", "py", "pz"]]
-)
-df_B_4mom_mc = four_momemtum_dataframe(
-    data[["mcE", "mcPX", "mcPY", "mcPZ"]]
-)
-df_ell_p_4mom = four_momemtum_dataframe(
-    data[[ell_p_E, ell_p_px, ell_p_py, ell_p_pz]]
-)
-df_ell_p_4mom_mc = four_momemtum_dataframe(
-    data[[ell_p_mcE, ell_p_mcPX, ell_p_mcPY, ell_p_mcPZ]]
-)
-df_ell_m_4mom = four_momemtum_dataframe(
-    data[[ell_m_E, ell_m_px, ell_m_py, ell_m_pz]]
-)
-df_ell_m_4mom_mc = four_momemtum_dataframe(
-    data[[ell_m_mcE, ell_m_mcPX, ell_m_mcPY, ell_m_mcPZ]]
-)
-df_K_4mom = four_momemtum_dataframe(
-    data[["K_p_E", "K_p_px", "K_p_py", "K_p_pz"]]
-)
-df_K_4mom_mc = four_momemtum_dataframe(
-    data[["K_p_mcE", "K_p_mcPX", "K_p_mcPY", "K_p_mcPZ"]]
-)
-df_KST_4mom = four_momemtum_dataframe(
-    data[["KST0_E", "KST0_px", "KST0_py", "KST0_pz"]]
-)
-df_KST_4mom_mc = four_momemtum_dataframe(
-    data[["KST0_mcE", "KST0_mcPX", "KST0_mcPY", "KST0_mcPZ"]]
-)
+    df_B_4mom = four_momemtum_dataframe(
+        data[["E", "px", "py", "pz"]]
+    )
+    df_B_4mom_mc = four_momemtum_dataframe(
+        data[["mcE", "mcPX", "mcPY", "mcPZ"]]
+    )
+    df_ell_p_4mom = four_momemtum_dataframe(
+        data[[ell_p_E, ell_p_px, ell_p_py, ell_p_pz]]
+    )
+    df_ell_p_4mom_mc = four_momemtum_dataframe(
+        data[[ell_p_mcE, ell_p_mcPX, ell_p_mcPY, ell_p_mcPZ]]
+    )
+    df_ell_m_4mom = four_momemtum_dataframe(
+        data[[ell_m_E, ell_m_px, ell_m_py, ell_m_pz]]
+    )
+    df_ell_m_4mom_mc = four_momemtum_dataframe(
+        data[[ell_m_mcE, ell_m_mcPX, ell_m_mcPY, ell_m_mcPZ]]
+    )
+    df_K_4mom = four_momemtum_dataframe(
+        data[["K_p_E", "K_p_px", "K_p_py", "K_p_pz"]]
+    )
+    df_K_4mom_mc = four_momemtum_dataframe(
+        data[["K_p_mcE", "K_p_mcPX", "K_p_mcPY", "K_p_mcPZ"]]
+    )
+    df_KST_4mom = four_momemtum_dataframe(
+        data[["KST0_E", "KST0_px", "KST0_py", "KST0_pz"]]
+    )
+    df_KST_4mom_mc = four_momemtum_dataframe(
+        data[["KST0_mcE", "KST0_mcPX", "KST0_mcPY", "KST0_mcPZ"]]
+    )
 
 
-data["q_squared"] = invariant_mass_squared_two_particles(
-    df_ell_p_4mom, df_ell_m_4mom
-)
-data["q_squared_mc"] = invariant_mass_squared_two_particles(
-    df_ell_p_4mom_mc, df_ell_m_4mom_mc
-)
-data[costheta_ell] = find_costheta_ell(
-    df_ell_p_4mom, df_ell_m_4mom, df_B_4mom
-)
-data[costheta_ell_mc] = find_costheta_ell(
-    df_ell_p_4mom_mc, df_ell_m_4mom_mc, df_B_4mom_mc
-)
-data["costheta_K"] = find_costheta_K(
-    df_K_4mom, df_KST_4mom, df_B_4mom
-)
-data["costheta_K_mc"] = find_costheta_K(
-    df_K_4mom_mc, df_KST_4mom_mc, df_B_4mom_mc
-)
-data["coschi"] = find_coschi(
-    df_B_4mom,
-    df_K_4mom,
-    df_KST_4mom,
-    df_ell_p_4mom,
-    df_ell_m_4mom,
-)
-data["coschi_mc"] = find_coschi(
-    df_B_4mom_mc,
-    df_K_4mom_mc,
-    df_KST_4mom_mc,
-    df_ell_p_4mom_mc,
-    df_ell_m_4mom_mc,
-)
-data["chi"] = find_chi(
-    df_B_4mom,
-    df_K_4mom,
-    df_KST_4mom,
-    df_ell_p_4mom,
-    df_ell_m_4mom,
-)
-data["chi_mc"] = find_chi(
-    df_B_4mom_mc,
-    df_K_4mom_mc,
-    df_KST_4mom_mc,
-    df_ell_p_4mom_mc,
-    df_ell_m_4mom_mc,
-)
+    data["q_squared"] = invariant_mass_squared_two_particles(
+        df_ell_p_4mom, df_ell_m_4mom
+    )
+    data["q_squared_mc"] = invariant_mass_squared_two_particles(
+        df_ell_p_4mom_mc, df_ell_m_4mom_mc
+    )
+    data[costheta_ell] = find_costheta_ell(
+        df_ell_p_4mom, df_ell_m_4mom, df_B_4mom
+    )
+    data[costheta_ell_mc] = find_costheta_ell(
+        df_ell_p_4mom_mc, df_ell_m_4mom_mc, df_B_4mom_mc
+    )
+    data["costheta_K"] = find_costheta_K(
+        df_K_4mom, df_KST_4mom, df_B_4mom
+    )
+    data["costheta_K_mc"] = find_costheta_K(
+        df_K_4mom_mc, df_KST_4mom_mc, df_B_4mom_mc
+    )
+    data["coschi"] = find_coschi(
+        df_B_4mom,
+        df_K_4mom,
+        df_KST_4mom,
+        df_ell_p_4mom,
+        df_ell_m_4mom,
+    )
+    data["coschi_mc"] = find_coschi(
+        df_B_4mom_mc,
+        df_K_4mom_mc,
+        df_KST_4mom_mc,
+        df_ell_p_4mom_mc,
+        df_ell_m_4mom_mc,
+    )
+    data["chi"] = find_chi(
+        df_B_4mom,
+        df_K_4mom,
+        df_KST_4mom,
+        df_ell_p_4mom,
+        df_ell_m_4mom,
+    )
+    data["chi_mc"] = find_chi(
+        df_B_4mom_mc,
+        df_K_4mom_mc,
+        df_KST_4mom_mc,
+        df_ell_p_4mom_mc,
+        df_ell_m_4mom_mc,
+    )
+    
+    return data
 
-data.to_pickle(out_path)
+
+input_paths, output_paths = config_paths(input_dir, output_dir)
+
+for in_path, out_path in zip(input_paths, output_paths):
+    data = open_data(in_path)
+    analyzed_data = run_calc(data)
+    analyzed_data.to_pickle(out_path)
+
