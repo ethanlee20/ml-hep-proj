fc1e6566270fa0853e81965425111eabafbe4aae
ethanlee20 <elee20@hawaii.edu>
2024-02-22 11:10:17 -1000

    analyze

--- scripts/peek/check.py
+++ scripts/peek/check.py
@@ -6,8 +6,8 @@ from mylib.util.util import open_data
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu_backup/BtoKstMuMu/analyzed/mu_re_00030_job386260858_00_cut_an.pkl'
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu/BtoKstMuMu/sub00/mu_re_00000_job386260828_00.root'
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu/BtoKstMuMu/sub00/mu_re_00001_job386260829_00.root'
-data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
-
+# data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
+data = '/home/belle2/elee20/ml-hep-proj/data/2024-02-21_e_test/ana/e_re_cut_an.pkl'
 print(data.head())
 print(data.columns.values.tolist())
-print(data["mcPDG"])
+# print(data["mcPDG"])
