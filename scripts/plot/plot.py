578a65680c94615b0913dbf83114c94bf4313dde
ethanlee20 <elee20@hawaii.edu>
2024-02-27 10:37:14 -1000

    afb and s5 err

--- scripts/plot/plot.py
+++ scripts/plot/plot.py
@@ -35,7 +35,8 @@ from mylib.plot.s5 import plot_s5
 setup_mpl_params_save()
 
 # data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-23_e_brems_test/ana')
-data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
+data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/')
+# data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
 out_dir = pl.Path('/home/belle2/elee20/ml-hep-proj/data/2024-02-23_e_brems_test/plots')
 
 # hist_chi(data, out_dir)
