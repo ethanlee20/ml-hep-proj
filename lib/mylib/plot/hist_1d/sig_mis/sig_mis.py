350333667cb9c5fd32dd31a6fdf5e63077be4385
ethanlee20 <elee20@hawaii.edu>
2024-02-27 12:50:10 -1000

    afb and s5 err

---
+++ lib/mylib/plot/hist_1d/sig_mis/sig_mis.py
@@ -0,0 +1,118 @@
+
+import matplotlib.pyplot as plt
+
+from mylib.plot.core.util.save import save
+from mylib.plot.core.looks.leg import stats_legend
+from mylib.util.data import approx_num_bins
+
+
+# def plot_signal_and_misrecon(
+#     data,
+#     variable, 
+#     q_squared_split, 
+#     reconstruction_level, 
+#     title, 
+#     xlabel, 
+#     out_dir_path, 
+#     **kwargs
+# ):
+#     data = pre.preprocess(
+#         data=data,
+#         variables=variable,
+#         q_squared_split=q_squared_split,
+#         reconstruction_level=reconstruction_level
+#     )
+
+#     signal = data[data["isSignal"] == 1]
+#     misrecon = data[data["isSignal"] == 0]
+
+#     signal_label = generate_stats_label(
+#         signal, 
+#         descrp="Signal",
+#     )
+#     misrecon_label = generate_stats_label(
+#         misrecon,
+#         descrp="Misrecon.",
+#         show_mean=False,
+#         show_rms=False,
+#     )
+
+#     bins = approx_num_bins(signal)
+
+#     fig, ax = plt.subplots()
+
+#     ax.hist(
+#         signal,
+#         label=signal_label,
+#         bins=bins,
+#         alpha=0.6,
+#         color="red",
+#         histtype="stepfilled",
+#         **kwargs,
+#     )
+
+#     ax.hist(
+#         misrecon,
+#         label=misrecon_label,
+#         bins=bins,
+#         color="blue",
+#         histtype="step",
+#         linewidth=1,
+#         **kwargs,
+#     )
+
+#     ax.legend()
+#     ax.set_title(title)
+#     ax.set_xlabel(xlabel)
+
+#     if variable == "chi":
+#         x_axis_in_radians(kind="0 to 2pi")
+            
+#     file_name = f'q2{q_squared_split}_{reconstruction_level}_{variable}.png'
+
+#     save_fig_and_clear(
+#         out_dir_path=out_dir_path,
+#         file_name=file_name,
+#     )
+
+
+def plot_deltaE(data, out_dir):
+    data = data["deltaE"]
+    det = data.loc["det"]
+    det_sig = det[det["isSignal"]==1]
+    det_mis = det[det["isSignal"]==0]
+    gen = data.loc["gen"]
+
+
+    leg_sig = stats_legend(det_sig, "Detector: Signal")
+    leg_mis = stats_legend(det_mis, "Detector: Misrecon.", show_mean=False, show_rms=False)
+    leg_gen = stats_legend(gen, "Generator")
+
+    n_bins = approx_num_bins(det_sig)
+
+    fig, ax = plt.subplots()
+
+    ax.hist(
+        det_sig,
+        label=leg_sig,
+        bins=n_bins,
+        alpha=0.6,
+        color="red",
+        histtype="stepfilled",
+    )
+
+    ax.hist(
+        det_mis,
+        label=leg_mis,
+        bins=n_bins,
+        color="blue",
+        histtype="step",
+        linewidth=1,
+    )
+
+    ax.set_title(r'$\Delta E$')
+    ax.set_xlabel(r'[GeV]')
+
+    save("deltaE", q_squared_split='all', out_dir=out_dir)
+
+
--- scripts/plot/plot.py
+++ scripts/plot/plot.py
@@ -5,11 +5,13 @@ from mylib.util.util import open_data, open_data_dir
 
 from mylib.plot.core.util.setup import setup_mpl_params_save
 
-from mylib.plot.hist_1d.chi import hist_chi
-from mylib.plot.hist_1d.costheta_K import hist_costheta_K
-from mylib.plot.hist_1d.costheta_e import hist_costheta_e
-from mylib.plot.hist_1d.q_squared import hist_q_squared
-from mylib.plot.hist_1d.theta_lab_k import hist_theta_lab_k
+# from mylib.plot.hist_1d.chi import hist_chi
+# from mylib.plot.hist_1d.costheta_K import hist_costheta_K
+# from mylib.plot.hist_1d.costheta_e import hist_costheta_e
+# from mylib.plot.hist_1d.q_squared import hist_q_squared
+# from mylib.plot.hist_1d.theta_lab_k import hist_theta_lab_k
+
+from mylib.plot.hist_1d.sig_mis.sig_mis import plot_deltaE
 
 from mylib.plot.afb import plot_afb
 from mylib.plot.s5 import plot_s5
@@ -34,11 +36,13 @@ from mylib.plot.s5 import plot_s5
 
 setup_mpl_params_save()
 
-# data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-23_e_brems_test/ana')
-data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/')
+data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-23_e_brems_test/ana')
+# data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/')
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
 out_dir = pl.Path('/home/belle2/elee20/ml-hep-proj/data/2024-02-23_e_brems_test/plots')
 
+
+plot_deltaE(data, out_dir)
 # hist_chi(data, out_dir)
 # hist_costheta_e(data, out_dir)
 # hist_costheta_K(data, out_dir)
@@ -46,8 +50,8 @@ out_dir = pl.Path('/home/belle2/elee20/ml-hep-proj/data/2024-02-23_e_brems_test/
 
 # hist_theta_lab_k(data, out_dir)
 
-plot_afb(data, out_dir)
-plot_s5(data, out_dir)
+# plot_afb(data, out_dir)
+# plot_s5(data, out_dir)
 # hist_2d_costheta_k_theta_k(data, out_dir)
 # hist_2d_costheta_k_p_k(data, out_dir)
 
