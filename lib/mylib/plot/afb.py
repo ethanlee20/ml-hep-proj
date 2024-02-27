7a4c59140f6d16703ef917e7cbab153e1740ec24
ethanlee20 <elee20@hawaii.edu>
2024-02-27 10:29:01 -1000

    afb and s5 err

--- lib/mylib/plot/afb.py
+++ lib/mylib/plot/afb.py
@@ -22,7 +22,7 @@ def plot_afb(data, out_dir):
     plt.errorbar(gen_x, gen_y, yerr=gen_err, fmt='none', ecolor='red', elinewidth=0.5, capsize=1, alpha=0.7)
     plt.scatter(gen_x, gen_y, s=4, color="red", alpha=0.7, label=leg_gen, marker="X")
     plt.errorbar(det_x, det_y, yerr=det_err, fmt='none', ecolor='blue', elinewidth=0.5, capsize=1, alpha=0.7)
-    plt.scatter(det_x, det_y, s=4, color="blue", alpha=1, label=leg_det, marker="d", alpha=0.7)
+    plt.scatter(det_x, det_y, s=4, color="blue", alpha=0.7, label=leg_det, marker="d")
     plt.ylim(-0.3, 0.5)
     plt.xlim(0, 19) 
     plt.title(r"$A_{FB}$ for $\ell = \mu$")
