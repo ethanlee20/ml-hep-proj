fbd723695bc02d8ee8cae2d350dc7720352f908f
ethanlee20 <elee20@hawaii.edu>
2024-02-29 10:30:40 -1000

    grid e cut plots

--- scripts/plot/plot.py
+++ scripts/plot/plot.py
@@ -37,7 +37,8 @@ from mylib.plot.hist_2d.deltaE_Mbc import hist2d_deltaE_mbc
 
 setup_mpl_params_save()
 
-data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/mc/BtoKstee2/sub00', tree_names=['gen', 'det'])
+# data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/mc/BtoKstee2/sub00', tree_names=['gen', 'det'])
+data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/mc/BtoKstee2/sub00/e_re_00051_job394437823_00.root', tree_names=['gen', 'det'])
 # data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/')
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
 out_dir = pl.Path('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/plots_uncut')
