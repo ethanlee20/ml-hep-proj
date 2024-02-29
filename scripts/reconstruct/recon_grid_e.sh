db5794ef28aca7418346817ae6087c467ff26587
ethanlee20 <elee20@hawaii.edu>
2024-02-28 14:38:12 -1000

    grid e

---
+++ scripts/reconstruct/recon_grid_e.sh
@@ -0,0 +1,6 @@
+
+gbasf2 \
+    -p BtoKstee \
+    -s release-06-01-09 \
+    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025630/s00/e1003/4S/r00000/1120240010/mdst \ 
+    /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/recon_steer_e.py
