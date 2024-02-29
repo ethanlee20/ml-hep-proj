fc41064743433c27b3b13a0cf762cf480f913eca
ethanlee20 <elee20@hawaii.edu>
2024-02-29 13:47:36 -1000

    grid e cut plots

--- scripts/plot/plot.py
+++ scripts/plot/plot.py
@@ -12,6 +12,7 @@ from mylib.plot.core.util.setup import setup_mpl_params_save
 # from mylib.plot.hist_1d.gen_det.theta_lab_k import hist_theta_lab_k
 
 from mylib.plot.hist_1d.basic import plot_deltaE, plot_mbc, plot_invM
+from mylib.plot.hist_1d.ang import hist_chi, hist_costheta_e, hist_costheta_K, hist_q_squared
 # from mylib.plot.hist_2d.deltaE_Mbc import hist2d_deltaE_mbc
 
 # from mylib.plot.afb import plot_afb
@@ -41,25 +42,19 @@ setup_mpl_params_save()
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/cut/e_re_00051_job394437823_00_cut.pkl', tree_names=['gen', 'det'])
 # data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/')
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
-data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/cut', tree_names=['gen', 'det'])
+data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/ana', tree_names=['gen', 'det'])
 out_dir = pl.Path('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/plots_cut')
-plot_deltaE(data, out_dir)
-plot_mbc(data, out_dir)
-plot_invM(data, out_dir)
-
-data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/mc/BtoKstee2/sub00', tree_names=['gen', 'det'])
-out_dir = pl.Path('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/plots_uncut')
-plot_deltaE(data, out_dir)
-plot_mbc(data, out_dir)
-plot_invM(data, out_dir)
+# plot_deltaE(data, out_dir)
+# plot_mbc(data, out_dir)
+# plot_invM(data, out_dir)
 
 
 # hist2d_deltaE_mbc(data, out_dir)
 
-# hist_chi(data, out_dir)
-# hist_costheta_e(data, out_dir)
-# hist_costheta_K(data, out_dir)
-# hist_q_squared(data, out_dir)
+hist_chi(data, out_dir)
+hist_costheta_e(data, out_dir)
+hist_costheta_K(data, out_dir)
+hist_q_squared(data, out_dir)
 
 # hist_theta_lab_k(data, out_dir)
 
