5de4c1f9620b973189c5ee2a918a4dd0477889a9
ethanlee20 <elee20@hawaii.edu>
2024-03-06 14:04:38 -1000

    peek

--- lib/mylib/plot/cand_mult.py
+++ lib/mylib/plot/cand_mult.py
@@ -1,21 +1,25 @@
 
+import matplotlib.pyplot as plt
+
+from mylib.plot.core.looks.leg import stats_legend
+from mylib.plot.core.util.save import save
 
 def plot_candidate_multiplicity(data, out_dir_path):
-    data_det = pre.preprocess(data, reconstruction_level="det")
-    data_gen = pre.preprocess(data, reconstruction_level="gen")
+    data_gen = data.loc["gen"]
+    data_det = data.loc["det"]
 
     plt.hist(
         data_gen["__event__"].value_counts().values, 
         bins=[-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5], 
-        label=generate_stats_label(data_gen["__event__"], descrp="Generator", show_mean=False, show_rms=False), 
+        label=stats_legend(data_gen["__event__"], descrp="Generator", show_mean=False, show_rms=False), 
         color="blue", 
-        histtype="step"
+        histtype="step" 
     )
 
     plt.hist(
         data_det["__event__"].value_counts().values, 
         bins=[-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5], 
-        label=generate_stats_label(data_det["__event__"], descrp="Detector (after cuts)", show_mean=False, show_rms=False),
+        label=stats_legend(data_det["__event__"], descrp="Detector (after cuts)", show_mean=False, show_rms=False),
         color="red", 
         histtype="step"
     )
@@ -25,7 +29,8 @@ def plot_candidate_multiplicity(data, out_dir_path):
     plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     plt.legend()
 
-    save_fig_and_clear(
-        out_dir_path=out_dir_path,
-        file_name="cand_mult.png",
+    save(
+        plot_name="cand_mult",
+        q_squared_split="all",
+        out_dir=out_dir_path
     )
--- scripts/peek/peek.py
+++ scripts/peek/peek.py
@@ -1,13 +1,16 @@
 import sys
 
-from mylib.util.util import open_data
+import pandas as pd
+pd.options.display.max_columns = None
+
+from mylib.util.util import open_data, open_data_dir
 
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu/BtoKstMuMu/sub00/mu_re.pkl'
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu_backup/BtoKstMuMu/analyzed/mu_re_00030_job386260858_00_cut_an.pkl'
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu/BtoKstMuMu/sub00/mu_re_00000_job386260828_00.root'
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu/BtoKstMuMu/sub00/mu_re_00001_job386260829_00.root'
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
-data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-02-21_e_test/ana/e_re_cut_an.pkl')
+data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/subset/cut/mu_re_00001_job388070870_00_cut.pkl')
 print(data.head())
 print(data.columns.values.tolist())
 # print(data["mcPDG"])
