f87f310b0e2211a25d712e0426dbce7db47270b8
ethanlee20 <elee20@hawaii.edu>
2024-03-07 12:41:00 -1000

    mu bdt

--- lib/mylib/plot/core/hist_2d/deltaE_Mbc.py
+++ lib/mylib/plot/core/hist_2d/deltaE_Mbc.py
@@ -12,6 +12,7 @@ def hist2d_deltaE_mbc(data, out_dir, sig_only=False):
 
     if sig_only:
         sig = data[data["isSignal"]==1].loc["det"]
+        breakpoint()
         plt.hist2d(sig["Mbc"], sig["deltaE"], bins=n_bins, cmap='hot', range=None)
         plt.colorbar()
         plt.title(r"Signal \small" + f"(count: {count_events(sig)})")
