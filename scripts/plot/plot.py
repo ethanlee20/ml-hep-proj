fa0b55cb968bf70eff58a96fd0c72633c320088c
ethanlee20 <elee20@hawaii.edu>
2024-03-07 17:03:09 -1000

    mu bdt

--- scripts/plot/plot.py
+++ scripts/plot/plot.py
@@ -49,7 +49,7 @@ setup_mpl_params_save()
 # data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/ana', tree_names=['gen', 'det'])
 # data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/subset/mc', tree_names=['gen', 'det'])
 # data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/subset/cut', tree_names=['gen', 'det'])
-data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/subset/cut', tree_names=['gen', 'det'])
+data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/subset/mc', tree_names=['gen', 'det'])
 
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-02-29_eGrid/ana/e_re_00051_job394437823_00_cut_an.pkl', tree_names=['gen', 'det'])
 
