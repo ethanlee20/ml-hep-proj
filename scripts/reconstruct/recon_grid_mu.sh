8463f3c3a7677b8ef8382b249170726c89857315
ethanlee20 <elee20@hawaii.edu>
2024-03-11 13:07:51 -1000

    bdt

--- scripts/reconstruct/recon_grid_mu.sh
+++ scripts/reconstruct/recon_grid_mu.sh
@@ -1,6 +1,6 @@
 
 gbasf2 \
-    -p genericMu \
+    -p genericMu2 \
     -s release-06-01-10 \
     -i /belle/MC/release-06-01-10/DB00002752/MC15rd_b/prod00029589/s00/e0018/4S/r00974/mixed/mdst \
     /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/recon_steer_mu.py
