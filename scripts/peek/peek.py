5e11a08757d458b99533be048c254fcc01569fb5
ethanlee20 <elee20@hawaii.edu>
2024-03-12 11:32:49 -1000

    bdt

--- scripts/peek/peek.py
+++ scripts/peek/peek.py
@@ -12,5 +12,7 @@ from mylib.util.util import open_data, open_data_dir
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
 data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-03-12_test_e/e_test4/sub00/e_re_00054_job397107243_00.root', tree_names=["gen", "det"])
 print(data.loc["gen"].head())
+print(len(data[data["isSignal"]==1]))
+print(len(data[data["isSignal"]==0]))
 # print(data.columns.values.tolist())
 # print(data["mcPDG"])
