1ee0730464e0f6537389fb19fca36693d23217dd
ethanlee20 <elee20@hawaii.edu>
2024-03-12 09:05:32 -1000

    bdt

--- scripts/peek/peek.py
+++ scripts/peek/peek.py
@@ -10,7 +10,7 @@ from mylib.util.util import open_data, open_data_dir
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu/BtoKstMuMu/sub00/mu_re_00000_job386260828_00.root'
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu/BtoKstMuMu/sub00/mu_re_00001_job386260829_00.root'
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
-data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-03-11_genericMu/genericMu/sub00/mu_re_00000_job396827188_00.root', tree_names=["gen", "det"])
+data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-03-12_generic_e/cuts', tree_names=["gen", "det"])
 print(data.loc["gen"].head())
 # print(data.columns.values.tolist())
 # print(data["mcPDG"])
