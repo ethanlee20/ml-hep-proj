8efc3ad31c07558bebdb9aca16ed811b42d4b61b
ethanlee20 <elee20@hawaii.edu>
2024-03-12 09:42:15 -1000

    bdt

--- scripts/reconstruct/recon_grid_e.sh
+++ scripts/reconstruct/recon_grid_e.sh
@@ -1,8 +1,13 @@
 
 gbasf2 \
-    -p generice \
+    -p e_test \
     -s release-06-01-10 \
-    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00024825/s00/e1003/4S/r00000/mixed/mdst \
+    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025630/s00/e1003/4S/r00000/1120240010/mdst \
     /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/recon_steer_e.py
     
+
+    # signal (10 mil events)
     # -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025630/s00/e1003/4S/r00000/1120240010/mdst \
+
+    # generic (100 mil events)
+    # /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00024825/s00/e1003/4S/r00000/mixed/mdst 
