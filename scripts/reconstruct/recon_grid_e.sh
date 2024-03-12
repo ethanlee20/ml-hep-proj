7b0b31bf6177d99ccf1c5c71571de4f6c282a361
ethanlee20 <elee20@hawaii.edu>
2024-03-11 14:42:06 -1000

    bdt

--- scripts/reconstruct/recon_grid_e.sh
+++ scripts/reconstruct/recon_grid_e.sh
@@ -1,6 +1,8 @@
 
 gbasf2 \
-    -p BtoKstee \
-    -s release-06-01-09 \
-    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025630/s00/e1003/4S/r00000/1120240010/mdst \
+    -p generice \
+    -s release-06-01-10 \
+    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00024825/s00/e1003/4S/r00000/mixed/mdst \
     /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/recon_steer_e.py
+    
+    # -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025630/s00/e1003/4S/r00000/1120240010/mdst \
