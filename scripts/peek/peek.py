47b0a9f11c1a1c4651a0b235a3d0721cec94cf0c
ethanlee20 <elee20@hawaii.edu>
2024-03-19 15:35:37 -1000

    bdt

--- scripts/peek/peek.py
+++ scripts/peek/peek.py
@@ -13,6 +13,7 @@ from mylib.util.util import open_data, open_data_dir
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu/BtoKstMuMu/sub00/mu_re_00001_job386260829_00.root'
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
 
+
 def open_data(path):
     path = pl.Path(path)
     if path.suffix in {".root", ".pkl"}:
@@ -31,9 +32,12 @@ def print_mc_particles(data):
 
 
 def print_counts(data):
+    isSignal = (data["isSignal"]==1) & (data["KST0_isSignal"]==1) & (data["K_p_isSignal"]==1) & (data["pi_m_isSignal"]==1) & (data["e_p_isSignal"]==1) & (data["e_m_isSignal"]==1)
+    isBkg = (data["isSignal"]==0) | (data["KST0_isSignal"]==0) | (data["K_p_isSignal"]==0) | (data["pi_m_isSignal"]==0) | (data["e_p_isSignal"]==0) | (data["e_m_isSignal"]==0)
+
     print("num gen", len(data.loc["gen"]))
-    print("num det sig", len(data.loc["det"][data.loc["det"]["isSignal"]==1]))
-    print("num det bkg", len(data.loc["det"][data.loc["det"]["isSignal"]==0]))
+    print("num det sig", len(data[isSignal].loc["det"]))
+    print("num det bkg", len(data[isBkg].loc["det"]))
     print("num det tot", len(data.loc["det"]))
 
 
