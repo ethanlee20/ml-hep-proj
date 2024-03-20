3705a8eb9b518fa18347f58ad23bbd53bde87b31
ethanlee20 <elee20@hawaii.edu>
2024-03-19 14:00:03 -1000

    bdt

--- scripts/peek/peek.py
+++ scripts/peek/peek.py
@@ -2,6 +2,7 @@ import sys
 
 import pandas as pd
 pd.options.display.max_columns = None
+pd.options.display.max_colwidth = None
 
 from mylib.util.util import open_data, open_data_dir
 
@@ -11,18 +12,21 @@ from mylib.util.util import open_data, open_data_dir
 #file_path = '/home/belle2/elee20/ml-hep-proj/data/2024-01-17_GridMu/BtoKstMuMu/sub00/mu_re_00001_job386260829_00.root'
 # data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-01-24_GridMu/BtoKstMuMu_theta/analyzed/mu_re_00003_job388070872_00_cut_an.pkl')
 data = open_data_dir('/home/belle2/elee20/ml-hep-proj/data/2024-03-19_gen_mix_e_print_test/gen_mix_e_print5/sub00', tree_names=["gen", "det"])
-# data.index = pd.MultiIndex.from_tuples(data.index)
-
-print(data.head())
-print(data.loc["det"].head())
 
 data_gen = data.loc["gen"]
 data_det = data.loc["det"]
 
-print("num gen", len(data_gen))
-print("num gen", len(data.loc["gen"]))
-print("num det sig", len(data_det[data_det["isSignal"]==1]))
-print("num det bkg", len(data_det[data_det["isSignal"]==0]))
-print("num det tot", len(data_det))
+# data.index = pd.MultiIndex.from_tuples(data.index)
+
+# print(data.head())
+# print(data.loc["det"].head())
+
+# print("num gen", len(data_gen))
+# print("num gen", len(data.loc["gen"]))
+# print("num det sig", len(data_det[data_det["isSignal"]==1]))
+# print("num det bkg", len(data_det[data_det["isSignal"]==0]))
+# print("num det tot", len(data_det))
+
+print(data_det["__MCDecayString__"])
 # print(data.columns.values.tolist())
 # print(data["mcPDG"])
