5485d0dcd160053474a49a49e69914d96d00fe83
ethanlee20 <elee20@hawaii.edu>
2024-03-20 10:48:08 -1000

    peek

--- scripts/reconstruct/recon_grid_e.sh
+++ scripts/reconstruct/recon_grid_e.sh
@@ -1,8 +1,8 @@
 
 gbasf2 \
-    -p gen_mix_e_new_cuts_test \
+    -p gen_charg_e_test \
     -s light-2401-ocicat \
-    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025056/s00/e1003/4S/r00000/mixed/mdst \
+    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00024816/s00/e1003/4S/r00000/charged/mdst \
     /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/recon_steer_e.py
     
 
