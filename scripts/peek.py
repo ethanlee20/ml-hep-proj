df6b3a549771a7f596f333dbe3102463f192dfd2
ethanlee20 <elee20@hawaii.edu>
2024-04-08 13:21:52 -1000

    peek

--- scripts/peek.py
+++ scripts/peek.py
@@ -35,7 +35,7 @@ def print_counts(data):
     print("num det tot", len(section(data, gen_det='det')))
 
     
-data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-03-30_bdt_15i/e/charge/an')
+data = open_data('/home/belle2/elee20/ml-hep-proj/data/2024-03-30_bdt_15i/e/mix/an')
 data_bkg_all = section(data, sig_noise='noise')
 data_bkg_jpsi = section(data, sig_noise='noise', q_squared_split='JPsi')
 data_bkg_psi2s = section(data, sig_noise='noise', q_squared_split='Psi2S')
