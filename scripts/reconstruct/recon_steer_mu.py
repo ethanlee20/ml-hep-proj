8b907004c6e02f86d6b1c7e3fbf76868e4065108
ethanlee20 <elee20@hawaii.edu>
2024-04-12 10:28:49 -1000

    mdst

--- scripts/reconstruct/recon_steer_mu.py
+++ scripts/reconstruct/recon_steer_mu.py
@@ -123,7 +123,7 @@ def save_output(B0_vars):
     
     mdst.add_mdst_output(
         path=main,
-        mc='True',
+        mc=True,
         filename=out_file_name + mdst_ext + root_ext,
     )
 
