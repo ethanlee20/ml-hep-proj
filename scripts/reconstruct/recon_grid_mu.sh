a2da3152027b7fbc3221189e2689d7dbd2a2c6ac
ethanlee20 <elee20@hawaii.edu>
2024-04-12 10:22:24 -1000

    mdst

--- scripts/reconstruct/recon_grid_mu.sh
+++ scripts/reconstruct/recon_grid_mu.sh
@@ -2,9 +2,14 @@
 gbasf2 \
     -p bkg_mix_mu_mdst \
     -s light-2401-ocicat \
-    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00024821/s00/e1003/4S/r00000/mixed/mdst \
+    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025631/s00/e1003/4S/r00000/1120240011/mdst \
     /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/recon_steer_mu.py
 
 
+    # signal
+    # -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025631/s00/e1003/4S/r00000/1120240011/mdst
+
+    # generic
+    # -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00024821/s00/e1003/4S/r00000/mixed/mdst
 
     # --input_dslist /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/lpns_results_gen_mixed.txt \
