156aef21075741266b65c8318370da9802748a55
ethanlee20 <elee20@hawaii.edu>
2024-04-18 11:11:23 -1000

    steering gb2 options

--- scripts/reconstruct/recon_steer.py
+++ scripts/reconstruct/recon_steer.py
@@ -16,6 +16,12 @@ ell = 'e'
 sideband = False
 cut_strength = 'tight'
 
+
+gb2_project = 'mu_mixed_mini_tightcuts'
+gb2_input_ds = '/belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00024821/s00/e1003/4S/r00000/mixed/mdst'
+gb2_setuprel = 'light-2401-ocicat'
+
+
 main = b2.Path()
 
 
