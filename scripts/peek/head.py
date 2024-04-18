55bf5619cb0c71154ca3a5173d9af13a60ecc90c
ethanlee20 <elee20@hawaii.edu>
2024-04-17 14:11:21 -1000

    cuts

--- scripts/peek/head.py
+++ scripts/peek/head.py
@@ -9,6 +9,7 @@ from mylib.util import open_data, section, veto_q_squared
 
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument("data_dir")
 parser.add_argument("--veto", action='store_true', help="veto out J/Psi and Psi(2S) regions of q squared")
 parser.add_argument("--bg_only", action='store_true', help="only include background events")
@@ -16,6 +17,13 @@ parser.add_argument("--sig_only", action='store_true', help="only include signal
 parser.add_argument('--gen', action='store_true', help="show generator level data")
 parser.add_argument('--det', action='store_true', help="show detector level data")
 parser.add_argument('--num_ex', type=int, default=5, help="number of examples to show")
+
+parser.add_argument("--cut_var", help="variable to cut on")
+parser.add_argument("--lower_bound", help="lower bound for cut", type=float)
+parser.add_argument("--upper_bound", help="upper bound for cut", type=float)
+parser.add_argument("--equal_to", help="equality to cut on", type=float)
+parser.add_argument("--not_equal_to", help="inequality to cut on", type=float)
+
 args = parser.parse_args()
 
 
@@ -33,6 +41,19 @@ if args.gen:
     data = section(data, gen_det='gen')
 elif args.det:
     data = section(data, gen_det='det')
+
+if args.cut_var:
+    assert (args.lower_bound is not None) | (args.upper_bound is not None) | (args.equal_to is not None) | (args.not_equal_to is not None), "must specify cut details"
+    if (args.lower_bound is not None) & (args.upper_bound is not None):
+        data = data[(data[args.cut_var] >= args.lower_bound) & (data[args.cut_var] <= args.upperbound)]
+    elif args.lower_bound is not None:
+        data = data[data[args.cut_var] >= args.lower_bound]
+    elif args.upper_bound is not None:
+        data = data[data[args.cut_var] <= args.upper_bound]
+    elif args.equal_to is not None:
+        data = data[data[args.cut_var] == args.equal_to]
+    elif args.not_equal_to is not None:
+        data = data[data[args.cut_var] != args.not_equal_to]
 
 
 print(data.head(args.num_ex))
