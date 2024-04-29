44796795e8aecadb86e348feb36dd34b0ccad518
ethanlee20 <elee20@hawaii.edu>
2024-04-29 12:21:52 -1000

    sl dec

--- scripts/simulate/decay_steering_sl_e.py
+++ scripts/simulate/decay_steering_sl_e.py
@@ -1,4 +1,5 @@
 import sys
+import pathlib as pl
 
 import basf2 as b2
 import generators as ge
@@ -10,9 +11,11 @@ import simulation as si
 main = b2.Path()
 
 
-path_dec = "/home/belle2/elee20/ml-hep-proj/scripts/simulate/sl_e.dec"
+path_dec = pl.Path("/home/belle2/elee20/ml-hep-proj/scripts/simulate/sl_e.dec")
 
-path_out = '/home/belle2/elee20/ml-hep-proj/data/2024-04-29_sl_e_test/mc_se_e.root'
+path_out = pl.Path('/home/belle2/elee20/ml-hep-proj/data/2024-04-29_sl_e_test/mc_se_e.root')
+path_out.mkdir(parents=True, exist_ok=True)
+
 
 
 main.add_module(
@@ -22,14 +25,14 @@ main.add_module(
 ge.add_evtgen_generator(
     path=main,
     finalstate="signal",
-    signaldecfile=path_dec,
+    signaldecfile=str(path_dec),
 )
 
 si.add_simulation(path=main)
 
 re.add_reconstruction(path=main)
 
-mdst.add_mdst_output(path=main, filename=path_out)
+mdst.add_mdst_output(path=main, filename=str(path_out))
 
 b2.process(path=main)
 
