68bad58a9eee0fdeca8d40026d0b8d7e77f5becd
ethanlee20 <elee20@hawaii.edu>
2024-05-04 11:18:34 -1000

    no vpho

--- scripts/reconstruct/recon_steer.py
+++ scripts/reconstruct/recon_steer.py
@@ -7,13 +7,15 @@
 Submit like this:
 
 gbasf2 \
-    -p e_sig_single4 \
+    -p e_gen_charge_sideb \
     -s light-2401-ocicat \
-    -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025630/s00/e1003/4S/r00000/1120240010/mdst/sub00/mdst_000001_prod00025630_task10020000001.root \
+    --input_dslist /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/lpns_results_gen_charged.txt \
     /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/recon_steer.py
 
     mixed bkg first file: -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00024821/s00/e1003/4S/r00000/mixed/mdst \
     mixed bkg mini list: --input_dslist /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/lpns_results_gen_mixed_mini.txt \
+    mixed bkg all list: --input_dslist /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/lpns_results_gen_mixed.txt \
+    charged bkg all list: --input_dslist /home/belle2/elee20/ml-hep-proj/scripts/reconstruct/lpns_results_gen_charged.txt \
     signal e first: -i /belle/MC/release-06-00-08/DB00002100/MC15ri_b/prod00025630/s00/e1003/4S/r00000/1120240010/mdst/sub00/mdst_000001_prod00025630_task10020000001.root \
 """
 
@@ -27,7 +29,7 @@ import vertex as vx
 
 
 ell = 'e'
-sideband = False
+sideband = True
 cut_strength = 'loose' # tight or loose
 
 
